2e445f5cc230461d66f72cc3b68ccef27ee68ab6
Your Name <you@example.com>
2025-05-08 10:46:07 +0545

    return back 	modified:   frontend/src/components/FriendCard.jsx

--- frontend/src/components/FriendCard.jsx
+++ frontend/src/components/FriendCard.jsx
@@ -1,30 +1,30 @@
 import { Link } from "react-router";
 import { LANGUAGE_TO_FLAG } from "../constants";
 
-const FriendCard = ({ friends }) => {
+const FriendCard = ({ friend }) => {
   return (
     <div className="card bg-base-200 hover:shadow-md transition-shadow">
       <div className="card-body p-4">
         {/* USER INFO */}
         <div className="flex items-center gap-3 mb-3">
           <div className="avatar size-12">
-            <img src={friends.profilePic} alt={friends.fullName} />
+            <img src={friend.profilePic} alt={friend.fullName} />
           </div>
-          <h3 className="font-semibold truncate">{friends.fullName}</h3>
+          <h3 className="font-semibold truncate">{friend.fullName}</h3>
         </div>
 
         <div className="flex flex-wrap gap-1.5 mb-3">
           <span className="badge badge-secondary text-xs">
-            {getLanguageFlag(friends.nativeLanguage)}
-            Native: {friends.nativeLanguage}
+            {getLanguageFlag(friend.nativeLanguage)}
+            Native: {friend.nativeLanguage}
           </span>
           <span className="badge badge-outline text-xs">
-            {getLanguageFlag(friends.learningLanguage)}
-            Learning: {friends.learningLanguage}
+            {getLanguageFlag(friend.learningLanguage)}
+            Learning: {friend.learningLanguage}
           </span>
         </div>
 
-        <Link to={`/chat/${friends._id}`} className="btn btn-outline w-full">
+        <Link to={`/chat/${friend._id}`} className="btn btn-outline w-full">
           Message
         </Link>
       </div>
